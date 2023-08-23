12475f0e554015bd15d92b7629dc2d0b76357a7c
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-23 11:21:54 +0300

    Update

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,18 +6,25 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-constructor ( ) { }
+  constructor() { }
   ngOnInit(): void {
   }
 
   protected = [
-   {id:1, name: 'Minimalists Analog Watch', price: '109', color: 'Black', available: 'Available', image:' /assets/products/product-image-1.jpeg'},
-   {id:2, name: 'Hisense Ultra HD Smart TV', price: '3339', color: 'Black', available: 'Available', image: '/assets/products/product-image-2.jpeg'},
-   {id:3, name: 'APPLE iPhone 12', price: '1855', color: 'Black', available: 'Not Available', image: '/assets/products/product-image-3.jpeg'},
-   {id:4, name: 'LG Fully Automatic Washing Machine', price: '1765', color: 'White', available: 'Available', image: '/assets/products/product-image-4.jpeg'},
-   {id:5, name: 'LG Refrigerator with Door Cooling', price: '2815', color: 'white', available:' Not Available', image: '/assets/products/product-image-5.jpeg'},
-   {id:6, name: 'DELL Inspiron One 27 Ryzen 7', price: '2145', color: 'White', available: 'Available', image: '/assets/products/product-image-6.jpeg'}
- ];
+    { name: 'Minimalists Analog Watch', price: '109', color: 'Black', available: 'Available', image: ' /assets/products/product-image-1.jpeg' },
+    { id: 2, name: 'Hisense Ultra HD Smart TV', price: '3339', color: 'Black', available: 'Available', image: '/assets/products/product-image-2.jpeg' },
+    { id: 3, name: 'APPLE iPhone 12', price: '1855', color: 'Black', available: 'Not Available', image: '/assets/products/product-image-3.jpeg' },
+    { id: 4, name: 'LG Fully Automatic Washing Machine', price: '1765', color: 'White', available: 'Available', image: '/assets/products/product-image-4.jpeg' },
+    { id: 5, name: 'LG Refrigerator with Door Cooling', price: '2815', color: 'white', available: ' Not Available', image: '/assets/products/product-image-5.jpeg' },
+    { id: 6, name: 'DELL Inspiron One 27 Ryzen 7', price: '2145', color: 'White', available: 'Available', image: '/assets/products/product-image-6.jpeg' }
+  ];
 }
 
-
+export interface X {
+  id?: number;
+  name: string;
+  price: string;
+  color: string;
+  available: string;
+  image: string
+}
